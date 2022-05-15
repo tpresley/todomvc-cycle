a138109e02a2edf85f500eb2d2c70920703f482f
Troy Presley <troy.presley@gmail.com>
2022-05-14 16:27:56 -1000

    refactor for new component API

--- src/app.jsx
+++ src/app.jsx
@@ -5,198 +5,169 @@ import { component } from 'cyclejs-component'
 import todos from './components/todos'
 
 
-const name = 'APP'
-
 
 const FILTER_LIST = ['all', 'active', 'completed']
 
 
-
-
-// INTENT FUNCTION   (optional, but needed to trigger any actions)
-// - takes the 'sources' object
-// - returns an object that maps 'intents' to 'actions'
-//   + intents are signals from outside the component (events from sources) that indicate something should happen
-//     * intents are just streams that 'fire' when an action should happen
-//     * they can optionally include relevent data to help perform the action later
-//   + actions are just a name/label for something that will happen
-//     * we don't *DO* anything here... we are just collecting a stream of 'actions' to do later
-//
-// sources:
-//  - STATE: stream of application state objects
-//    + automatically added by @cycle/state
-//    + send reducer functions to the state sink to update state (see @cycle/state documentation for details)
-//    + new state is emitted on the state source
-//  - DOM: searchable collection of DOM elements
-//    + search for elements by DOM selector using the .select() method
-//    + after running .select(), get a stream of events by running .events()
-//  - DOMFX: side effects for DOM elements (mainly focusing or changing the value of input fields)
-//    + no source events
-//    + send commands with the selctor, command, and data
-//  - ROUTER: add page hash routes and receive a stream of routing events
-//    + pass one or more paths to the route sink to set up routes
-//    + routing events for the paths you sent will then be emitted to the route source
-//  - STORE: access to get or set data to local storage
-//    + use .get(key) to fetch values
-//    + send objects of shape {key: 'key', value: 'value'} to the store sink to save to local storage
-//    + data is automatically converted to and from JSON
-//
-function intent({ STATE, DOM, ROUTER, STORE }) {
-
-  // fetch stored todos from local storage
-  //  - init to an empty array if no todos were found
-  //  - only take the first event to prevent reloading after storing todos
-  const store$           = STORE.get('todos', [])
-
-  // collect required DOM events and elements
-  const toggleAll$       = DOM.select('.toggle-all').events('click')
-  const clearCompleted$  = DOM.select('.clear-completed').events('click')
-  const input$           = DOM.select('.new-todo')
-
-  // get events from the input field
-  //  - the inputEvents helper returns common events and automatically returns the current value
-  const { value$, enter$ } = inputEvents(input$)
-
-  // create a stream of titles whenever a new todo is submitted
-  // - wait until the 'enter' key is pressed
-  // - map the event stream to just the 'title' (current value of the input)
-  // - filter out blank titles
-  const newTodo$ = enter$.compose(sampleCombine(value$))
-                         .map(([_, title]) => title.trim())
-                         .filter(title => title !== '')
-
-  // add routes to handle filtering based on browser path
-  const route$ = xs.fromArray(FILTER_LIST)
-
-  // save todos to localStorage whenever the app state changes
-  // - ignore the first state event to prevent storing the initialization data
-  const toStore$ = STATE.stream.drop(1)
-
-
-  return {
-    VISIBILITY:      ROUTER,
-    FROM_STORE:      store$,
-    NEW_TODO:        newTodo$,
-    TOGGLE_ALL:      toggleAll$,
-    CLEAR_COMPLETED: clearCompleted$,
-    ADD_ROUTE:       route$,
-    TO_STORE:        toStore$,
-  }
+// filter functions for each visibility option
+const filters = {
+  all:       todo => true,
+  active:    todo => !todo.completed,
+  completed: todo => todo.completed
 }
 
 
-const model = {
 
-  VISIBILITY: { STATE: (state, data) => ({ ...state, visibility: data }) },
+export default component({
+  name: 'APP',
 
-  FROM_STORE: { STATE: (state, data) => ({ ...state, todos: data }) },
+  initialState: {
+    visibility: 'all',
+    todos: []
+  },
 
-  NEW_TODO: { STATE: (state, data, next) => {
-    // calculate next id
-    const nextId = newId(state.todos)
+  calculated: {
+    total:     (state) => state.todos.length,
+    remaining: (state) => state.todos.filter(todo => !todo.completed).length,
+    completed: (state) => state.todos.filter(todo => todo.completed).length,
+    allDone:   (state) => state.todos.every(todo => todo.completed),
+  },
 
-    // send a new action to clear the new todo field
-    next('CLEAR_FORM')
+  children: { todos },
 
-    // add the new todo to the state
-    return {
+  model: {
+    VISIBILITY: (state, visibility) => ({
       ...state,
-      todos: [
-        ...state.todos,
-        {
-          id: nextId,
-          title: data,
-          completed: false
-        }
-      ]
+      visibility,
+      todos: state.todos.map(todo => ({ ...todo, hidden: !filters[visibility](todo) }))
+    }),
+
+    FROM_STORE: (state, data) => ({ ...state, todos: data }),
+
+    NEW_TODO: (state, data, next) => {
+      // calculate next id
+      const nextId = newId(state.todos)
+
+      // send a new action to clear the new todo field
+      next('CLEAR_FORM')
+
+      // add the new todo to the state
+      return {
+        ...state,
+        todos: [
+          ...state.todos,
+          {
+            id: nextId,
+            title: data,
+            completed: false
+          }
+        ]
+      }
+    },
+
+    TOGGLE_ALL: (state) => {
+      const allDone = state.todos.every(todo => todo.completed)
+      const todos   = state.todos.map(todo => ({ ...todo, completed: !allDone }))
+      return {...state, todos }
+    },
+
+    CLEAR_COMPLETED: (state) => {
+      const todos = state.todos.filter(todo => !todo.completed)
+      return { ...state, todos }
+    },
+
+    CLEAR_FORM: { DOMFX: ({ type: 'SET_VALUE', data: { selector: '.new-todo' } }) },
+
+    ADD_ROUTE: { ROUTER: true },
+
+    TO_STORE: { STORE: (state, data) => {
+      // sanitize todo objects
+      const todos = state.todos.map(({ id, title, completed }) => ({ id, title, completed }))
+      return { key: 'todos', value: todos }
+    } },
+  },
+
+  intent: ({ STATE, DOM, ROUTER, STORE }) => {
+
+    // fetch stored todos from local storage
+    //  - init to an empty array if no todos were found
+    //  - only take the first event to prevent reloading after storing todos
+    const store$           = STORE.get('todos', [])
+
+    const toggleAll$       = DOM.select('.toggle-all').events('click')
+    const clearCompleted$  = DOM.select('.clear-completed').events('click')
+    const input$           = DOM.select('.new-todo')
+
+    // get events from the input field
+    //  - the inputEvents helper returns common events and automatically returns the current value
+    const { value$, enter$ } = inputEvents(input$)
+
+    // create a stream of titles whenever a new todo is submitted
+    // - wait until the 'enter' key is pressed
+    // - map the event stream to just the 'title' (current value of the input)
+    // - filter out blank titles
+    const newTodo$ = enter$.compose(sampleCombine(value$))
+                           .map(([_, title]) => title.trim())
+                           .filter(title => title !== '')
+
+    // add routes to handle filtering based on browser path
+    const route$ = xs.fromArray(FILTER_LIST)
+
+    // save todos to localStorage whenever the app state changes
+    // - ignore the first state event to prevent storing the initialization data
+    const toStore$ = STATE.stream.drop(1)
+
+
+    return {
+      VISIBILITY:      ROUTER,
+      FROM_STORE:      store$,
+      NEW_TODO:        newTodo$,
+      TOGGLE_ALL:      toggleAll$,
+      CLEAR_COMPLETED: clearCompleted$,
+      ADD_ROUTE:       route$,
+      TO_STORE:        toStore$,
     }
-  } },
+  },
 
-  TOGGLE_ALL: { STATE: (state) => {
-    const allDone = state.todos.every(todo => todo.completed)
-    const todos   = state.todos.map(todo => ({ ...todo, completed: !allDone }))
-    return {...state, todos }
-  } },
+  view: ({ state, todos }) => {
+    const { visibility, total, remaining, completed, allDone } = state
+    // use the list of filters to generate links in footer
+    const links      = FILTER_LIST
 
-  CLEAR_COMPLETED: { STATE: (state) => {
-    const todos = state.todos.filter(todo => !todo.completed)
-    return { ...state, todos }
-  } },
+    const capitalize = word => word.charAt(0).toUpperCase() + word.slice(1)
+    const renderLink = link => <li><a href={ `#/${link}` } className={ classes({ selected: visibility == link }) }>{ capitalize(link) }</a></li>
 
-  CLEAR_FORM: { DOMFX: ({ type: 'SET_VALUE', data: { selector: '.new-todo' } }) },
+    return (
+      <section className="todoapp">
+        <header className="header">
+          <h1>todos</h1>
+          <input className="new-todo" autofocus autocomplete="off" placeholder="What needs to be done?" />
+        </header>
 
-  ADD_ROUTE: { ROUTER: true },
+        { (total > 0) &&
+          <section className="main">
+            <input id="toggle-all" className="toggle-all" type="checkbox" checked={ allDone } />
+            <label for="toggle-all">Mark all as complete</label>
+            <ul className="todo-list">
+              { todos }
+            </ul>
+          </section>
+        }
 
-  TO_STORE: { STORE: (state, data) => {
-    // sanitize todo objects
-    const todos = state.todos.map(({ id, title, completed }) => ({ id, title, completed }))
-    return { key: 'todos', value: todos }
-  } },
+        { (total > 0) &&
+          <footer className="footer">
+            <span className="todo-count">
+              <strong>{ remaining }</strong> { (remaining === 1) ? 'item' : 'items' } left
+            </span>
+            <ul className="filters">
+              { links.map(renderLink) }
+            </ul>
+            { (completed > 0) && <button className="clear-completed">Clear completed</button> }
+          </footer>
+        }
 
-}
+      </section>
+    )
+  }
 
-
-function view({ state, todos }) {
-  // total todos
-  const total      = state.todos.length
-  // number of todos that haven't been marked as complete
-  const remaining  = state.todos.filter(todo => !todo.completed).length
-  // number of completed todos
-  const completed  = total - remaining
-  // are all todos completed?
-  const allDone    = remaining === 0
-  // current filter setting
-  const visibility = state.visibility
-  // use the list of filters to generate links in footer
-  const links      = FILTER_LIST
-
-  const capitalize = word => word.charAt(0).toUpperCase() + word.slice(1)
-  const renderLink = link => <li><a href={ `#/${link}` } className={ classes({ selected: visibility == link }) }>{ capitalize(link) }</a></li>
-
-  return (
-    <section className="todoapp">
-      <header className="header">
-        <h1>todos</h1>
-        <input className="new-todo" autofocus autocomplete="off" placeholder="What needs to be done?" />
-      </header>
-
-      { (total > 0) &&
-        <section className="main">
-          <input id="toggle-all" className="toggle-all" type="checkbox" checked={ allDone } />
-          <label for="toggle-all">Mark all as complete</label>
-          <ul className="todo-list">
-            { todos }
-          </ul>
-        </section>
-      }
-
-      { (total > 0) &&
-        <footer className="footer">
-          <span className="todo-count">
-            <strong>{ remaining }</strong> { (remaining === 1) ? 'item' : 'items' } left
-          </span>
-          <ul className="filters">
-            { links.map(renderLink) }
-          </ul>
-          { (completed > 0) && <button className="clear-completed">Clear completed</button> }
-        </footer>
-      }
-
-    </section>
-  )
-}
-
-
-
-
-// initial state of the application
-const initialState     = {
-  visibility: 'all',
-  todos: []
-}
-
-const children = {
-  todos
-}
-
-export default component({ name, intent, model, view, children, initialState })
+})
--- src/components/todos.jsx
+++ src/components/todos.jsx
@@ -4,132 +4,116 @@ import { component, collection } from 'cyclejs-component'
 import { inputEvents, classes } from '../lib/utils'
 
 
-const name = 'TODO'
 
 
+const todo = component({
+  name: 'TODO',
+
+  model: {
+
+    TOGGLE:     (state) => ({ ...state, completed: !state.completed }),
+    DESTROY:    (state) => undefined,
+
+    EDIT_START: (state, data, next) => {
+      const selector = '.todo-' + state.id + ' .edit'
+      // update the value of the input field to the current todo title
+      next('SET_EDIT_VALUE',   { selector, value: state.title })
+      // set focus on the input field
+      next('FOCUS_EDIT_FIELD', { selector })
+      // mark the todo as being edited and save the current title in case the edit is cancelled
+      return { ...state, editing: true, cachedTitle: state.title }
+    },
+
+    EDIT_DONE: (state, data) => {
+      // if the todo is not being edited then don't change
+      if (state.editing === false) return state
+      // update the todo's title, remove the editing flag, and delete the cached title
+      return { ...state, title: data, editing: false, cachedTitle: '' }
+    },
+
+    EDIT_CANCEL: (state, done, next) => {
+      const selector = '.todo-' + state.id + ' .edit'
+      // set the value of the edit input field back to the original title
+      next('SET_EDIT_VALUE', { selector: selector, value: state.cachedTitle })
+      // set the todo back to the pre-edit value and remove the editing flag
+      return { ...state, title: state.cachedTitle, editing: false, cachedTitle: '' }
+    },
+
+    SET_EDIT_VALUE:   { DOMFX: (state, data) => ({ type: 'SET_VALUE', data }) },
+
+    FOCUS_EDIT_FIELD: { DOMFX: (state, data) => ({ type: 'FOCUS', data }) },
+
+  },
+
+  intent: ({ DOM }) => {
+    // collect DOM events and elements
+    const toggle$   = DOM.select('.toggle').events('click')
+    const label$    = DOM.select('.todo label').events('dblclick')
+    const destroy$  = DOM.select('.destroy').events('click')
+    const input$    = DOM.select('.edit')
+
+    // get events from the input field
+    //  - the inputEvents helper returns common events and automatically returns the current value
+    const { value$, enter$, escape$, blur$ } = inputEvents(input$)
+
+    // map submitted edits to the new title
+    const doneEditing$ = xs.merge(enter$, blur$)
+                           .compose(sampleCombine(value$))
+                           .map(([_, title]) => title)
 
 
-function intent({ DOM }) {
-  // collect DOM events and elements
-  const toggle$   = DOM.select('.toggle').events('click')
-  const label$    = DOM.select('.todo label').events('dblclick')
-  const destroy$  = DOM.select('.destroy').events('click')
-  const input$    = DOM.select('.edit')
+    return {
+      TOGGLE:      toggle$,
+      DESTROY:     destroy$,
+      EDIT_START:  label$,
+      EDIT_DONE:   doneEditing$,
+      EDIT_CANCEL: escape$,
+    }
+  },
 
-  // get events from the input field
-  //  - the inputEvents helper returns common events and automatically returns the current value
-  const { value$, enter$, escape$, blur$ } = inputEvents(input$)
+  view: ({ state }) => {
+    const { id, hidden, completed, editing, title } = state
+    if (hidden) return
+    // calculate class for todo
+    const classNames = classes('todo', 'todo-' + id, { completed, editing })
 
-  // map submitted edits to the new title
-  const doneEditing$ = xs.merge(enter$, blur$)
-                         .compose(sampleCombine(value$))
-                         .map(([_, title]) => title)
+    // is the todo completed?
+    const checked = !!completed
 
-
-  return {
-    TOGGLE:      toggle$,
-    DESTROY:     destroy$,
-    EDIT_START:  label$,
-    EDIT_DONE:   doneEditing$,
-    EDIT_CANCEL: escape$,
+    return (
+      <li className={ classNames }>
+        <div className="view">
+          <input className="toggle" type="checkbox" checked={ checked } />
+          <label>{ title }</label>
+          <button className="destroy" />
+        </div>
+        <input className="edit" type="text" value={ title } />
+      </li>
+    )
   }
-}
 
-
-const model = {
-
-  TOGGLE:     { STATE: (state) => ({ ...state, completed: !state.completed }) },
-
-  DESTROY:    { STATE: (state) => ({ ...state, deleted: true }) },
-
-  EDIT_START: { STATE: (state, data, next) => {
-    const selector = '.todo-' + state.id + ' .edit'
-    // update the value of the input field to the current todo title
-    next('SET_EDIT_VALUE',   { selector, value: state.title })
-    // set focus on the input field
-    next('FOCUS_EDIT_FIELD', { selector })
-    // mark the todo as being edited and save the current title in case the edit is cancelled
-    return { ...state, editing: true, cachedTitle: state.title }
-  } },
-
-  EDIT_DONE: { STATE: (state, data) => {
-    // if the todo is not being edited then don't change
-    if (state.editing === false) return state
-    // update the todo's title, remove the editing flag, and delete the cached title
-    return { ...state, title: data, editing: false, cachedTitle: '' }
-  } },
-
-  EDIT_CANCEL: { STATE: (state, done, next) => {
-    const selector = '.todo-' + state.id + ' .edit'
-    // set the value of the edit input field back to the original title
-    next('SET_EDIT_VALUE', { selector: selector, value: state.cachedTitle })
-    // set the todo back to the pre-edit value and remove the editing flag
-    return { ...state, title: state.cachedTitle, editing: false, cachedTitle: '' }
-  } },
-
-  SET_EDIT_VALUE:   { DOMFX: (state, data) => ({ type: 'SET_VALUE', data }) },
-
-  FOCUS_EDIT_FIELD: { DOMFX: (state, data) => ({ type: 'FOCUS', data }) },
-
-}
-
-
-function view({ state }) {
-  const { id, hidden, completed, editing, title } = state
-  if (hidden) return
-  // calculate class for todo
-  const classNames = classes('todo', 'todo-' + id, { completed, editing })
-
-  // is the todo completed?
-  const checked = !!completed
-
-  return (
-    <li className={ classNames }>
-      <div className="view">
-        <input className="toggle" type="checkbox" checked={ checked } />
-        <label>{ title }</label>
-        <button className="destroy" />
-      </div>
-      <input className="edit" type="text" value={ title } />
-    </li>
-  )
-}
+})
 
 
 
 
-const todo = component({ name, intent, model, view })
 
-// filter functions for each visibility option
-const filters = {
-  all:       todo => true,
-  active:    todo => !todo.completed,
-  completed: todo => todo.completed
-}
+// const todo = component({ name, intent, model, view })
+
 
 // state lense for getting and setting state for the todo collection
-const lense = {
-  get: state => {
-    // filter based on selected visibility
-    //  - makeCollection uses reference checks to determine changes so
-    //    for best performance, return the same object if you don't want
-    //    to re-render, and return a new object if you do
-    return state.todos.map(todo => {
-      return filters[state.visibility](todo) ? todo : { ...todo, hidden: true }
-    })
-  },
-  set: (state, childState) => {
-    return {
-      ...state,
-      // replace todos in main list with the filtered ones from the collection
-      // and remove any todos marked as deleted
-      todos: state.todos.map(todo => {
-        const newTodo = childState.find(ctodo => ctodo.id === todo.id)
-        return newTodo || todo
-      }).filter(todo => !todo.deleted)
-    }
-  }
-}
+// const lense = {
+//   get: state => {
+//     // filter based on selected visibility
+//     //  - makeCollection uses reference checks to determine changes so
+//     //    for best performance, return the same object if you don't want
+//     //    to re-render, and return a new object if you do
+//     return state.todos.map(todo => {
+//       return filters[state.visibility](todo) ? todo : { ...todo, hidden: true }
+//     })
+//   },
+//   set: (state, childState) => ({ ...state, todos: [...childState] })
+// }
 
 // build a new 'collection' component of 'todo' components
 // - the resulting collection will automatically grow and shink with changes to the state
@@ -137,4 +121,4 @@ const lense = {
 // - returns an instantiable 'component', so can be included in the 'children' parameter of the component() function
 // - for very simple applications the 'lense' parameter can be a string specifying an array in the state
 //   but most applications quickly get to the point they need a 'lense' like above
-export default collection(todo, lense)
+export default collection(todo, 'todos')
